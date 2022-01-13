1a3c8523adf3cff9177c528ad0231cf02df48d87
Zhiliaev Dmitrii <dmitrii.zhiliaev@t-systems.ru>
2022-01-13 19:22:52 +0300

    new test case for yandex main page

--- cypress/integration/myTests/my1stTestCy.js
+++ cypress/integration/myTests/my1stTestCy.js
@@ -1,18 +1,17 @@
 describe('mytest', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('https://yandex.ru')
   })
 
-  it('displays two todo items by default', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('div.home-logo__default').should('have.length', 1)
+  it('Check yandex main page', () => {
 
+    cy.get('div.services-new__icon').should('have.length', 9)
+
+    cy.get('div.services-new__item-title').should('have.length', 10)
+    cy.get('div.services-new__item-title').first().should("have.text",'Маркет')
+    cy.get('div.services-new__item-title').last().should("have.text",'ещё')
+
+    cy.get('.input__input.mini-suggest__input').type(`'T-Systems'{enter}`)
     // We can go even further and check that the default todos each contain
     // the correct text. We use the `first` and `last` functions
     // to get just the first and last matched elements individually,
